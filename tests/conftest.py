425bdfe8fe88574c54982e2d4ae9f278832e04c9
Daniel Jordan <daniel.jordan@fellow-consulting.de>
2020-07-15 14:39:13 +0200

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.inforion import inforion
+#
 
 """
 #from TSIClient import TSIClient as tsi
