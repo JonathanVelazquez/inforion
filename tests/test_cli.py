076e8fb300734ceabbc60600600243957f4fa51a
Roberto Metz <robertometz@robertometz.com>
2020-07-19 16:55:52 -0400

    Update test_cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,8 @@
-import os
 import uuid
 
 import pytest
 from click.testing import CliRunner
+
 from inforion.__main__ import create
 from inforion.__main__ import datalake_get
 from inforion.__main__ import datalake_list
