ca984798fdf9ff52558358246859af26566e58b6
Roberto Metz <roberto.metz@fellow-consulting.de>
2020-07-21 13:07:16 -0400

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,17 +9,10 @@ from inforion.__main__ import datalake_list
 from inforion.__main__ import delete
 from inforion.__main__ import upload
 
-__credentials_file = os.path.abspath(
-    os.path.join(os.getcwd(), "credentials", "credentials.ionapi")
-)
-__data_file = os.path.abspath(os.path.join(os.getcwd(), "data", "sample.csv"))
-__schema_file = os.path.abspath(
-    os.path.join(os.getcwd(), "data", "catalog_schema.json")
-)
-__properties_file = os.path.abspath(
-    os.path.join(os.getcwd(), "data", "catalog_properties.json")
-)
-
+__credentials_file = "credentials/credentials.ionapi"
+__data_file = "data/sample.csv"
+__schema_file = "data/catalog_schema.json"
+__properties_file = "data/catalog_properties.json"
 
 def test_catalog_create():
     runner = CliRunner()
