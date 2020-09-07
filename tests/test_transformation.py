ee49952f9a9d9e4aa42d329afe2a7c3d05296735
Restyled.io <commits@restyled.io>
2020-09-07 09:46:39 +0000

    Restyled by yapf

--- tests/test_transformation.py
+++ tests/test_transformation.py
@@ -30,9 +30,8 @@ class TestTransformationMethods(unittest.TestCase):
         staged_data.rename(columns={field: "No_1"}, inplace=True)
         print(staged_data)
         with self.assertRaises(TransformationError) as context:
-            parallelize_tranformation(
-                self.valid_mappingfile, "Artikel", staged_data, None, 1
-            )
+            parallelize_tranformation(self.valid_mappingfile, "Artikel",
+                                      staged_data, None, 1)
 
         self.assertEqual(
             context.exception.message,
@@ -42,9 +41,8 @@ class TestTransformationMethods(unittest.TestCase):
     def test_validate_tabs(self):
         tab = "STAT"
         with self.assertRaises(TransformationError) as context:
-            parallelize_tranformation(
-                self.invalid_mappingfile, "Artikel", self.staging_data, None, 1
-            )
+            parallelize_tranformation(self.invalid_mappingfile, "Artikel",
+                                      self.staging_data, None, 1)
 
         self.assertEqual(
             context.exception.message,
@@ -52,17 +50,17 @@ class TestTransformationMethods(unittest.TestCase):
         )
 
     def test_validate_transformation(self):
-        df_return = parallelize_tranformation(
-            self.valid_mappingfile, "Artikel", self.staging_data, None, 1
-        )
+        df_return = parallelize_tranformation(self.valid_mappingfile,
+                                              "Artikel", self.staging_data,
+                                              None, 1)
         df_return = df_return.applymap(str)
 
         pdt.assert_frame_equal(self.sample_output, df_return)
 
     def test_validate_wildcard(self):
-        df_return = parallelize_tranformation(
-            self.valid_mappingfile, "Artikel-Lagerort", self.staging_wc_data, None, 1
-        )
+        df_return = parallelize_tranformation(self.valid_mappingfile,
+                                              "Artikel-Lagerort",
+                                              self.staging_wc_data, None, 1)
         df_return = df_return.applymap(str)
 
         pdt.assert_frame_equal(self.sample_wc_output, df_return)
@@ -70,12 +68,14 @@ class TestTransformationMethods(unittest.TestCase):
     def setUp(self):
         self.staging_wc_data = pd.read_excel(self.staging_wc_file)
         self.staging_data = pd.read_excel(self.staging_file, dtype=str)
-        self.sample_output = pd.read_excel(self.sample_otputfile, dtype=str).replace(
-            np.nan, "", regex=True
-        )
-        self.sample_wc_output = pd.read_excel(self.sample_wc_file, dtype=str).replace(
-            np.nan, "", regex=True
-        )
+        self.sample_output = pd.read_excel(self.sample_otputfile,
+                                           dtype=str).replace(np.nan,
+                                                              "",
+                                                              regex=True)
+        self.sample_wc_output = pd.read_excel(self.sample_wc_file,
+                                              dtype=str).replace(np.nan,
+                                                                 "",
+                                                                 regex=True)
 
     def tearDown(self):
         pass
